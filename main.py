09d04e96f57f01a902806ef7e0e72dcf511a8924
Tejas Kadam <kadamtejasravindra@gmail.com>
2025-04-28 10:36:18 +0530

    Adding chat history

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import gradio as gr
 
+history = []
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=groq_api_key)
@@ -21,13 +22,23 @@ def transcribe(audio_file):
         return "Could not understand the audio."
    
 def voice_bot(text):
+   
+   history.append({"role":"user","content":text})
    chat_completion = client.chat.completions.create(
-   messages = [{"role":"user", "content":text}],
+   messages = history,
    model = "llama-3.3-70b-versatile"
    )
-   return chat_completion.choices[0].message.content
+
+   bot_response = chat_completion.choices[0].message.content
+   history.append({"role":"assistant","content":bot_response})
+   print(history)
+   return bot_response
+
    
-    
+def clear_history():
+    global history
+    history = []
+    return "History Cleared"
 
 
 with gr.Blocks() as demo:
@@ -41,11 +52,8 @@ with gr.Blocks() as demo:
    transcribe_btn.click(fn = transcribe , inputs = audio_input , outputs = text_input).then(
    fn=voice_bot,inputs = text_input,outputs = processed_output)
 
-
-
-
-
-
+   reset_btn = gr.Button("Reset Chat")
+   reset_btn.click(fn=clear_history, inputs=[], outputs=processed_output)
 
 
 
